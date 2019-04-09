e1654493573335e8dfea2813f818105bfcd2fa5e
raosahebmore <42617461+raosahebmore@users.noreply.github.com>
2019-04-10 01:56:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from urllib.parse import quote as urlquote
 from flask import Flask, send_from_directory
 import dash
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import fitz
